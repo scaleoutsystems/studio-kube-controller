cdd5ac99b29b50b317ebe8ac311bc5da5d4c60d6
Fredrik Wrede <fredrik@scaleoutsystems.com>
2023-04-27 18:22:16 +0200

    get status of pod

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8.10-minimal
+FROM python:3.8.10-alpine
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
--- event_listener.py
+++ event_listener.py
@@ -1,16 +1,28 @@
 from kubernetes import client, config, watch
 
-config.incluster_config.load_incluster_config()
 
-api = client.CoreV1Api()
-w = watch.Watch()
+def init_event_listener():
+    config.incluster_config.load_incluster_config()
 
-label_selector = "type=app"
-namespace = "default"
+    api = client.CoreV1Api()
+    w = watch.Watch()
 
-for event in w.stream(
-    api.list_namespaced_pod(namespace=namespace, label_selector=label_selector)
-):
-    pod = event["object"]
-    print(f"METADATA: {pod.metadata}", flush=True)
-    print(f"EVENT_TYPE: {event['type']}", flush=True)
+    label_selector = "type=app"
+    namespace = "default"
+
+    for event in w.stream(api.list_namespaced_pod, namespace=namespace, label_selector=label_selector):
+        pod = event["object"]
+        status = pod.status.phase
+        release = pod.metadata.labels["release"]
+        print(f"EVENT_TYPE: {event['type']}", flush=True)
+        print(f"STATUS: {status}", flush=True)
+        send_status_to_rest_api(release, status)
+
+def send_status_to_rest_api(release, status):
+    """
+    TODO: Send the updated status phase to studio REST-API endpoint
+    """
+    print(f"UPDATE: {release} to {status}", flush=True)
+
+if __name__ == '__main__':
+    init_event_listener()
